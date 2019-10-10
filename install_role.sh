68dd1ef30f4014936eb73599a5231795227a5386
Geonmo Ryu <geonmo@kisti.re.kr>
2019-10-10 08:17:02 +0000

    fix typo.

--- install_role.sh
+++ install_role.sh
@@ -4,8 +4,8 @@ sudo yum update -y ansible
 
 ansible-galaxy -c install geerlingguy.repo-epel
 ansible-galaxy -c install hephyvienna.htcondor_ce
-ansible-galaxy -c install hephyvienna.htcondor_grid
-ansible-galaxy -c install hephyvienna.htcondor_poolaccounts
+ansible-galaxy -c install hephyvienna.grid
+ansible-galaxy -c install hephyvienna.poolaccounts
 ansible-galaxy -c install hephyvienna.workernode
 ansible-galaxy -c install geonmo.ansible_htcondor
 
