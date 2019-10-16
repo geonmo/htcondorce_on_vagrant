b3d8d48bfa7e07f0961969d672f1f74da04aed3c
Geonmo Ryu <geonmo@kisti.re.kr>
2019-10-16 06:08:52 +0000

    Add order and limit.

--- test/apel/check_apel.sh
+++ test/apel/check_apel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "EventRecords"
-mysql -u apel -pSomeSecretPasswordNooneCanGuess -h node3.intranet.local -e 'use apelclient; select JobName,StartTime,EndTime from EventRecords;'
+mysql -u apel -pSomeSecretPasswordNooneCanGuess -h node3.intranet.local -e 'use apelclient; select JobName,StartTime,EndTime from EventRecords order by EndTime desc LIMIT 10;'
 echo "BlahdRecords"
-mysql -u apel -pSomeSecretPasswordNooneCanGuess -h node3.intranet.local -e 'use apelclient; select TimeStamp,GlobalJobId,LrmsId from BlahdRecords;'
+mysql -u apel -pSomeSecretPasswordNooneCanGuess -h node3.intranet.local -e 'use apelclient; select TimeStamp,GlobalJobId,LrmsId from BlahdRecords order by TimeStamp desc LIMIT 10;'
 echo "SpecRecords"
 mysql -u apel -pSomeSecretPasswordNooneCanGuess -h node3.intranet.local -e 'use apelclient; select * from SpecRecords;'
 echo "JobRecords"
-mysql -u apel -pSomeSecretPasswordNooneCanGuess -h node3.intranet.local -e 'use apelclient; select UpdateTime,LocalJobId,StartTime,EndTime from JobRecords;'
+mysql -u apel -pSomeSecretPasswordNooneCanGuess -h node3.intranet.local -e 'use apelclient; select UpdateTime,LocalJobId,StartTime,EndTime from JobRecords order by UpdateTime desc LIMIT 10;'
