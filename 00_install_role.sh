ff2bd6a75791f44dcd434a40198b62b272a9b16e
Geonmo Ryu <geonmo@kisti.re.kr>
2019-10-16 05:09:25 +0000

    Patch for other sites.

--- 00_install_role.sh
+++ 00_install_role.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum update -y ansible
+#sudo yum update -y
+#sudo yum update -y ansible
 
 ansible-galaxy -c install geerlingguy.repo-epel
 ansible-galaxy -c install hephyvienna.htcondor_ce
@@ -10,5 +10,8 @@ ansible-galaxy -c install hephyvienna.workernode
 ansible-galaxy -c install geonmo.ansible_htcondor
 
 
-./remote_install_rootca.sh
+## Dueto KISTI security, 
+#./remote_install_rootca.sh
+
+
 ./create-all.sh
