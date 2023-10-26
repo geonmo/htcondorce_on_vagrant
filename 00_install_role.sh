c25986ffc74ccf26582c41369691bf976d0521bb
Geonmo Ryu <geonmo@kisti.re.kr>
2023-10-26 04:31:37 +0000

    htcondor role 경로 변경 처리

--- 00_install_role.sh
+++ 00_install_role.sh
@@ -4,7 +4,7 @@
 
 
 ansible-galaxy role install geerlingguy.repo-epel
-ansible-galaxy role install geonmo.ansible_htcondor
+ansible-galaxy role install geonmo.htcondor
 ansible-galaxy role install geonmo.grid
 ansible-galaxy role install geonmo.htcondor_ce
 
